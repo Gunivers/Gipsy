204244a5bf03c2f5086227e79c587d7a9fdd0ca2
Z_runner <z.runner.mc@gmail.com>
2021-02-24 08:40:32 +0100

    add `/kill` cmd

--- plugins/misc.py
+++ plugins/misc.py
@@ -51,6 +51,28 @@ class Misc(commands.Cog):
     async def dataja(self, ctx: MyContext):
         """Don't ask to ask, just ask."""
         await ctx.send(await self.bot._(ctx.guild.id, 'misc.dataja'))
+    
+
+    @commands.command(name="kill")
+    async def kill(self, ctx: MyContext, *, target: str=None):
+        """Wanna kill someone?"""
+        if target is None: # victim is user
+            victime = ctx.author.display_name
+            ex = ctx.author.display_name.replace(" ","_")
+        else: # victim is target
+            victime = target
+            ex = target.replace(" ","_")
+        author = ctx.author.mention
+        tries = 0
+        # now let's find a random answer
+        msg = 'misc.kills'
+        while msg.startswith('misc.kills') or ('{0}' in msg and target is None and tries<50):
+            choice = random.randint(0, 23)
+            msg = await self.bot._(ctx.channel, f"misc.kills.{choice}")
+            tries += 1
+        # and send it
+        await ctx.send(msg.format(author, victime, ex), allowed_mentions=discord.AllowedMentions.none())
+
 
 # The end.
 def setup(bot):

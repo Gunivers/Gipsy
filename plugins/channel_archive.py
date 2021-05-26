d77cfa720571c28f649012a32c9572561758cb20
Leirof <vince.lrf@gmail.com>
2021-05-26 13:36:12 +0200

    Archive channel - Completed

--- plugins/channel_archive.py
+++ plugins/channel_archive.py
@@ -16,15 +16,38 @@ class Channel_Archive(commands.Cog):
 
     @commands.command(name="archive")
     @commands.guild_only()
-    async def archive(self, ctx: MyContext):
+    async def archive(self, ctx: MyContext, channel: discord.TextChannel=None):
         """Archive a channel"""
 
-        embed = discord.Embed(
-            description="Le salon a bien été déplacé même si en fait non pck pour le moment ça marche pas ...",
-            colour=discord.Colour(51711)
-        )
+        # Get archive category
+        config = self.bot.server_configs[ctx.guild.id]
+        archive = self.bot.get_channel(config["archive_category"])
 
+        # Get target channel
+        if channel is None:
+            channel = ctx.channel
+
+        # Check permissions
+        if ctx.author.permissions_in(channel).manage_channels is True and ctx.author.permissions_in(channel).manage_permissions is True:
+
+            # Move channel
+            await channel.move(beginning=True, category=archive, sync_permissions=True, reason="Channel archived")
+
+            # Success message
+            embed = discord.Embed(
+                description=await self.bot._(ctx.guild.id, 'archive_channel.success', channel=channel.mention),
+                colour=discord.Colour(51711)
+            )
+        else:
+
+            # Missing permission message
+            embed = discord.Embed(
+                description=await self.bot._(ctx.guild.id, 'archive_channel.missing_permission'),
+                colour=0x992d22
+            )
+        
         await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Channel_Archive(bot))

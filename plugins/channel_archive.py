26dc472489e7e4277104a120e660dfc3f09d30d9
Leirof <vince.lrf@gmail.com>
2021-05-26 18:53:53 +0200

    clear & list archived channels

--- plugins/channel_archive.py
+++ plugins/channel_archive.py
@@ -8,11 +8,66 @@ class Channel_Archive(commands.Cog):
     def __init__(self, bot: Gunibot):
         self.bot = bot
         self.file = "channel_archive"
-        self.config_options = ['archive_category']
+        self.config_options = ['archive_category','archive_duration']
 
     #-------------------#
     # Commande /archive #
     #-------------------#
+
+    @commands.command(name="list_archive")
+    @commands.guild_only()
+    async def list_archive(self, ctx: MyContext):
+
+        # Get records
+        query = f"SELECT * FROM archive WHERE guild = {ctx.guild.id}"
+        records = self.bot.db_query(query, ())
+
+        # Print each record
+        i = 0
+        message = ""
+        if len(records) > 0:
+            for record in records:
+                i+=1
+                if i is not 1:
+                    message += "\n"
+                if self.bot.get_channel(record["channel"]) is not None:
+                    message += self.bot.get_channel(record["channel"]).mention + " - " + record["timestamp"]
+                else:
+                    message += "#deleted-channel - " + record["timestamp"]
+            await ctx.send(embed=discord.Embed(description=message,colour=discord.Colour.red()))
+
+        else:
+            await ctx.send(embed=discord.Embed(description=await self.bot._(ctx.guild.id, 'archive_channel.no_channel'),colour=discord.Colour.red()))
+
+    @commands.command(name="clear_archive")
+    @commands.guild_only()
+    async def clear_archive(self, ctx: MyContext):
+
+        # Get archive duration
+        config = self.bot.server_configs[ctx.guild.id]
+        duration = config["archive_duration"]
+
+        # Get & delete old channels
+        query = f"SELECT channel FROM archive WHERE timestamp <= datetime('now','-{duration} seconds')"
+        records = self.bot.db_query(query, ())
+        res = 0
+        for record in records:
+            if self.bot.get_channel(record["channel"]) is not None:
+                res += 1
+                await self.bot.get_channel(record["channel"]).delete(reason="Exceeded archive duration.")
+
+        # Clear database
+        query = f"DELETE FROM archive WHERE timestamp <= datetime('now','-{duration} seconds')"
+        self.bot.db_query(query, ())
+
+        # Send confirmation
+        if res is 0:
+            await ctx.send(embed=discord.Embed(description=await self.bot._(ctx.guild.id, 'archive_channel.no_deleted'),colour=discord.Colour.blue()))
+        elif res is 1:
+            await ctx.send(embed=discord.Embed(description=await self.bot._(ctx.guild.id, 'archive_channel.one_deleted'),colour=discord.Colour.green()))
+        else:
+            await ctx.send(embed=discord.Embed(description=await self.bot._(ctx.guild.id, 'archive_channel.several_deleted', count=res),colour=discord.Colour.green()))
+
 
     @commands.command(name="archive")
     @commands.guild_only()
@@ -32,6 +87,10 @@ class Channel_Archive(commands.Cog):
 
             # Move channel
             await channel.move(beginning=True, category=archive, sync_permissions=True, reason="Channel archived")
+
+            # Add record to database
+            query = "INSERT INTO archive (guild, channel) VALUES (?, ?)"
+            self.bot.db_query(query, (ctx.guild.id, channel.id))
 
             # Success message
             embed = discord.Embed(

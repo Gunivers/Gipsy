1d5cd241b338572d5aa995e2dc97cae8c0b8d902
Raphaël Ribes <raphael.ribes@altearn.xyz>
2021-06-09 19:34:59 +0200

    Quizz sql model

--- data/model.sql
+++ data/model.sql
@@ -97,3 +97,11 @@ CREATE TABLE IF NOT EXISTS `archive` (
   `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX IF NOT EXISTS idx_archive_guild ON `archive` (`guild`);
+
+CREATE TABLE IF NOT EXISTS `quizz` (
+  `guild` BIGINT NOT NULL,
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `question` MEDIUMTEXT,
+  `awnser` MEDIUMTEXT
+);
+CREATE INDEX IF NOT EXISTS idx_archive_guild ON `quizz` (`guild`);
